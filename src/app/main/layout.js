09fba3f67eaca2acc02bcb1d6422247aa4c7b1fc
Muhammad Afrasyab <afrasyab.dev@gmail.com>
2025-03-02 21:49:03 +0500

    Fixed the chat scrolling issue

--- src/app/main/layout.js
+++ src/app/main/layout.js
@@ -3,11 +3,8 @@ export default function RootLayout({ children }) {
   return (
     <div className="flex h-screen w-full">
       <Sidebar />
-
-      <div className="flex-1 flex flex-col w-full">
         <div className="flex-1 w-full overflow-y-scroll">
           <div className="">{children}</div>
-        </div>
       </div>
     </div>
   );
